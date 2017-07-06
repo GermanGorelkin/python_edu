23d6500eabd9a2c6ca3e471a72c5dd7582e505fe
German Gorelkin <german.gorelkin@gmail.com>
2017-07-06 16:39:45 +0300

    add base route in hello

--- flask/1/hello.py
+++ flask/1/hello.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    header_info = ''
+    for k, v in request.headers.items():
+        header_info += '<p>{0}: {1}</p>'.format(k, v)
+    return header_info
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {0}<h1>'.format(name)
+
+
+@app.route('/set-cookie')
+def set_cookie():
+    response = make_response('<h1>body</h1>')
+    response.set_cookie('answer', '42')
+    response.status_code = 200
+    return response
+
+
+@app.route('/redir/<rtype>')
+def redirect(rtype):
+    if rtype == '1':
+        response = make_response('')
+        response.headers['Location'] = 'http://127.0.0.1:5000/'
+        response.status_code = 302
+        return response
+    elif rtype == '2':
+        return redirect('http://www.example.com')
+    else:
+        return ''
 
 
 if __name__ == '__main__':
