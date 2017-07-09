87eee3e112e7c9c7d886f17280b3d8dd0a24a7b8
German Gorelkin <german.gorelkin@gmail.com>
2017-07-09 19:37:36 +0300

    testing os.walk

---
+++ base_op/os/tree.py
@@ -0,0 +1,21 @@
+import os
+
+
+def list_files(start_path):
+    for root, dirs, files in os.walk(start_path):
+        level = root.replace(start_path, '').count(os.sep)
+        indent = ' ' * 4 * level
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+start_path = '/'
+# list_files(start_path)
+
+for root, dirs, files in os.walk(start_path, topdown=False):
+    path = root.split(os.sep)
+    print((len(path) - 1) * '---', os.path.basename(root))
+    for file in files:
+        print(len(path) * '---', file)
+
