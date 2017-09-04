1d2bcf5df472f669d4c75f1ca8a6d6c8f58d80e2
German Gorelkin <german.gorelkin@gmail.com>
2017-09-04 22:59:19 +0300

    add classic pattern Iterator v1

---
+++ base_op/patterns/Iterator/classic_iterator.py
@@ -0,0 +1,85 @@
+import abc
+
+
+class Aggregate(abc.ABC):
+
+    @abc.abstractmethod
+    def iterator(self):
+        """
+        Возращает итератор
+        """
+        pass
+
+
+class Iterator(abc.ABC):
+    def __init__(self, collection, cursor):
+        self._collection = collection
+        self._cursor = cursor
+
+    @abc.abstractmethod
+    def first(self):
+        """
+        Возращает итератор к началу агрегата
+        """
+        pass
+
+    @abc.abstractmethod
+    def next(self):
+        """
+        Переходит на следующий элемент агрегата.
+        Вызывает ошибку StopIteration, если достигнут конец последовательности.
+        """
+        pass
+
+    @abc.abstractmethod
+    def current(self):
+        """
+        Возвращает текущий элемент
+        """
+        pass
+
+
+class ListIterator(Iterator):
+    def __init__(self, collection, cursor):
+        super().__init__(collection, cursor)
+
+    def first(self):
+        self._cursor = -1
+
+    def next(self):
+        if self._cursor + 1 >= len(self._collection):
+            raise StopIteration()
+        self._cursor += 1
+
+    def current(self):
+        return self._collection[self._cursor]
+
+
+class ListCollection(Aggregate):
+    def __init__(self, collection):
+        self._collection = collection
+
+    def iterator(self):
+        return ListIterator(self._collection, -1)
+
+
+if __name__ == '__main__':
+    collection = [1, 2, 5, 6, 8]
+    aggregate = ListCollection(collection)
+    iter = aggregate.iterator()
+
+    while True:
+        try:
+            iter.next()
+        except StopIteration:
+            break
+        print(iter.current())
+
+    iter.first()
+
+    while True:
+        try:
+            iter.next()
+        except StopIteration:
+            break
+        print(iter.current())
