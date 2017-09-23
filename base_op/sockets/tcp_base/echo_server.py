c3453656a5dc40f071bf8686c3bcc97a9c3dac51
German Gorelkin <german.gorelkin@gmail.com>
2017-09-23 10:56:32 +0300

    add simple tcp socket server

---
+++ base_op/sockets/tcp_base/echo_server.py
@@ -0,0 +1,34 @@
+import socket
+from time import ctime
+
+HOST = ''
+PORT = 21568
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+print(socket.gethostbyname(socket.gethostname()))
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcpServSock:
+    tcpServSock.bind(ADDR)
+    tcpServSock.listen()
+    print(tcpServSock.getsockname())
+
+    while True:
+        print('waiting for connection...')
+        tcpClisock, addr = tcpServSock.accept()
+        print('...connected from:', addr)
+
+        try:
+            while True:
+                data = tcpClisock.recv(BUFSIZ)
+                if not data:
+                    break
+                data = '[{time}] {data}'.format(time=ctime(), data=data.decode('utf-8'))
+                print(data)
+                data = data.encode('utf-8')
+                tcpClisock.send(data)
+        except:
+            pass
+        finally:
+            tcpClisock.close()
+
